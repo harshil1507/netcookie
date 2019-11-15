fbd15c0d91f4831e9eed87153bc737f41320df23
Harshil maniar <32811532+harshil1507@users.noreply.github.com>
2019-11-15 06:55:27 +0530

    cookie file

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookie
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie ck = new HttpCookie("cookie_name");
+            ck.Value = "This is the value of the cookie";
+            Response.Cookies.Add(ck);
+            var t = Response.Cookies["cookie_name"].Value;
+            Label1.Text = t;
+        }
+    }
+}
